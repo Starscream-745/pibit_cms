b2b29fcb5e8b948655e384b0874a85e666f74697
Star <besttyagi.cr7@gmail.com>
2026-04-30 17:17:06 +0530

    chore: ignore all .md files and remove trailing cursor effect

--- frontend/src/components/CursorSwitcher.tsx
+++ frontend/src/components/CursorSwitcher.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { useTheme } from '../contexts/ThemeContext';
-import TrailingCursor from './GhostCursor';
 import BallCursor from './BallCursor';
 
 const CursorSwitcher: React.FC = () => {
-  const { theme } = useTheme();
-
   return (
     <>
-      {theme === 'dark' ? <BallCursor /> : <TrailingCursor />}
+      <BallCursor />
     </>
   );
 };
