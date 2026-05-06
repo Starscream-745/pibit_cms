6d988fa7a3b9a43a59dc9b75093a776af23be193
Star <besttyagi.cr7@gmail.com>
2026-05-06 15:24:05 +0530

    feat: remove all cursor effects and mouse-reactive background orbs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,29 +17,17 @@ import LoginPage from './pages/LoginPage';
 import AnalyticsDashboard from './pages/AnalyticsDashboard';
 import UserManagement from './pages/UserManagement';
 import Preloader from './components/Preloader';
-import CursorSwitcher from './components/CursorSwitcher';
 
 const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 800); 
     
-    const handleMouseMove = (e: MouseEvent) => {
-      // Calculate normalized mouse position (-1 to 1)
-      const x = (e.clientX / window.innerWidth - 0.5) * 2;
-      const y = (e.clientY / window.innerHeight - 0.5) * 2;
-      setMousePos({ x, y });
-    };
-
-    window.addEventListener('mousemove', handleMouseMove);
-
     return () => {
       clearTimeout(timer);
-      window.removeEventListener('mousemove', handleMouseMove);
     };
   }, []);
 
@@ -47,14 +35,10 @@ const App: React.FC = () => {
     <ThemeProvider>
       <AuthProvider>
         <ToastProvider>
-          <div className="bg-orb bg-orb-1" style={{ transform: `translate(${mousePos.x * -30}px, ${mousePos.y * -30}px)` }} />
-          <div className="bg-orb bg-orb-2" style={{ transform: `translate(${mousePos.x * 20}px, ${mousePos.y * 20}px)` }} />
-          <div className="bg-orb bg-orb-3" style={{ transform: `translate(${mousePos.x * -15}px, ${mousePos.y * -15}px)` }} />
           <div className="bg-geo bg-geo-ring-1" />
           <div className="bg-geo bg-geo-ring-2" />
           <div className="bg-geo bg-geo-diamond" />
           <Preloader isLoading={isLoading} />
-          <CursorSwitcher />
           <Router>
         <Routes>
           {/* Public routes - No login required */}
